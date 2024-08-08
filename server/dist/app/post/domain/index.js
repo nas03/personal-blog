b9533f920953c6c7ba138be8ae505a4ce589e524
anhson <sonanhnguyen003@gmail.com>
2024-08-09 02:51:50 +0700

    remove dist

--- server/dist/app/post/domain/index.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-// Business Logic
--- server/dist/app/user/domain/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.auth = void 0;
-const auth_1 = __importDefault(require("../../../app/user/domain/auth"));
-exports.auth = auth_1.default;
